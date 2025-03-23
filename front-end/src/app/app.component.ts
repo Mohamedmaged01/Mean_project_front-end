dd24564eae7834c4655a5b65afbedc02934931d9
AHMED739268 <mo017886@gmail.com>
2025-03-23 23:15:26 +0200

    last version

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -3,9 +3,10 @@ import { RouterOutlet } from '@angular/router';
 import {LoginComponent} from './Components/user/login/login.component';
 import {SignupComponent} from './Components/user/signup/signup.component';
 import{ListWishlistComponent} from './Components/user/list-wishlist/list-wishlist.component';
+import{CartComponent} from './Components/user/cart/cart.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,LoginComponent,SignupComponent,ListWishlistComponent],
+  imports: [RouterOutlet,LoginComponent,SignupComponent,ListWishlistComponent,CartComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
